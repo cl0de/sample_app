4edd337c61ad165a283d40b6cbe51e1caa4551c9
Clotilde <uwaclode@gmail.com>
2015-03-05 23:04:47 +0000

    Finish user edit, update and destroy actions

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150302134821) do
+ActiveRecord::Schema.define(version: 20150305215215) do
 
-# Could not dump table "users" because of following NoMethodError
-#   undefined method `[]' for nil:NilClass
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.string   "password_digest"
+    t.string   "remember_digest"
+    t.boolean  "admin",           default: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
